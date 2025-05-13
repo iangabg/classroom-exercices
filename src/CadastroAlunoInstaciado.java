cbcc9643e51a5fa4ee068f7fa50eaa16ee80fd5b
iangabg <xxluiz1227@gmail.com>
2025-05-13 10:46:12 -0300

    feat:Classe Main do Cadastro de Aluno

---
+++ src/CadastroAlunoInstaciado.java
@@ -0,0 +1,11 @@
+public class CadastroAlunoInstaciado {
+    public static void main(String[] args) {
+
+        Aluno ian = new Aluno("ian", 20);
+        Aluno lucca = new Aluno("lucca", 20);
+
+        ian.exibirDados();
+        lucca.exibirDados();
+
+    }
+}
